0c05c91ac3a7bbee3781f7a7057e9952019baa65
Oluwatobiloba Agunloye <61969703+Xemicolon@users.noreply.github.com>
2021-04-14 22:54:44 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 exports.db = async () => {
+  try {
   const db_connect = await mongoose.connect(
     process.env.NODE_ENV === "production" ? process.env.DB_URI : process.env.DB_URL,
     {
@@ -8,12 +9,16 @@ exports.db = async () => {
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-      user: process.env.NODE === "production" ? process.env.mongoUser : "",
-      pass: process.env.NODE === "production" ? process.env.mongoPass : "",
+//       user: process.env.NODE === "production" ? process.env.mongoUser : "",
+//       pass: process.env.NODE === "production" ? process.env.mongoPass : "",
     }
   );
 
   console.log(
     `Database Connected\nDatabase Url: ${db_connect.connection.host}`
   );
+  } catch(err) {
+    console.log(err)
+    return err
+  }
 };
